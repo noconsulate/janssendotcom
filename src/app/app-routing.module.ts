45b1f212a70e48571ff08663b8052a5982123bad
Janssen Kuhn <noconsulate@gmail.com>
2021-09-04 10:46:38 -0600

    routes and components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BiographyComponent } from './biography/biography.component'
+import { PortfolioComponent } from './portfolio/portfolio.component'
+import { RepositoryComponent } from './repository/repository.component'
+import { ContactComponent } from './contact/contact.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'biography', component: BiographyComponent },
+  { path: 'portfolio', component: PortfolioComponent },
+  { path: 'repository', component: RepositoryComponent },
+  { path: 'contact', component: ContactComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BiographyComponent } from './biography/biography.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { RepositoryComponent } from './repository/repository.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BiographyComponent
+    BiographyComponent,
+    PortfolioComponent,
+    RepositoryComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
